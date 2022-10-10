652d67a7664cd18cb2f5fd006d6b29bf871960eb
liamcannon <liam.cannon@mymail.champlain.edu>
2022-10-10 15:20:59 -0400

    updated constraint/domain

--- calendar.py
+++ calendar.py
@@ -21,15 +21,27 @@ class Professor(NamedTuple):
 
 class Course(NamedTuple):
     name: str
-    room: Room
     doubleBlock: bool
-    professor: Professor
 # Schedule class
 
-class Schedule(NamedTuple):
-    schedule: List[List[Course]]
-    
+class RoomCourse(NamedTuple):
+    room: Room
+    course: Course
+    # Need to add when we update domain to contain professors
+    # professor: Professor
 
+class Schedule(NamedTuple):
+    schedule: List[List[RoomCourse]]
+
+def dict_to_schedule(variables, dict, schedule):
+    newSched = deepcopy(schedule.schedule)
+    list_of_domains = [dict[i].schedule for i in variables]
+    for i in list_of_domains:
+        for x in range(len(i)):
+            for y in range(len(i[0])):
+                if i[x][y] != "-":
+                    newSched[x][y] = i[x][y]
+    return newSched
 
 def generate_grid(rows: int, columns: int) -> Schedule:
     # initialize grid with random letters
@@ -46,16 +58,50 @@ def generate_domain(course: Course, schedule: Schedule) -> List[Schedule]:
 
     SCHEDULE_WIDTH = len(schedule.schedule)
     SCHEDULE_LENGTH = len(schedule.schedule[0])
+    rooms: List[str] = [Room("JOYC 201"), Room("JOYC 210"), Room("JOYC 211"), Room("MIC 308")]
 
 
     domain: List[Schedule] = []
 
     # if(schedule.doubleBlock):
         # generate domain for double block courses (1x2)
-        
+    for room in rooms:
+        roomCourse = RoomCourse(room, course)
+        if course.doubleBlock:    
+            for row in range(SCHEDULE_WIDTH):
+                for column in range(SCHEDULE_LENGTH):
+                    if(column + 1 <= SCHEDULE_LENGTH-1):
+                        if(schedule.schedule[row][column] == "-" and schedule.schedule[row][column + 1] == "-"):
+                            tempCopy = deepcopy(schedule)
+                            tempCopy.schedule[row][column] = roomCourse
+                            tempCopy.schedule[row][column + 1] = roomCourse
+                            domain.append(tempCopy)
+        else:
+            # generate domain for single block courses (1x1)
+            for row in range(2):
+                for column in range(len(schedule.schedule[0])):
+                    if(schedule.schedule[row][column] == "-"):
+                        tempCopy = deepcopy(schedule)
+                        tempCopy.schedule[row][column] = roomCourse
+                        tempCopy.schedule[row+2][column] = roomCourse
+                        domain.append(tempCopy)
+    return domain
+
+""" def OLD_generate_domain(course: Course, schedule: Schedule) -> List[Schedule]:
+
+    SCHEDULE_WIDTH = len(schedule.schedule)
+    SCHEDULE_LENGTH = len(schedule.schedule[0])
+
+
+
+    domain: List[Schedule] = []
+
+    # if(schedule.doubleBlock):
+        # generate domain for double block courses (1x2)
+    
     if course.doubleBlock:    
-        for row in range(len(schedule.schedule)):
-            for column in range(len(schedule.schedule[row])):
+        for row in range(SCHEDULE_WIDTH):
+            for column in range(SCHEDULE_LENGTH):
                 if(column + 1 <= SCHEDULE_LENGTH-1):
                     if(schedule.schedule[row][column] == "-" and schedule.schedule[row][column + 1] == "-"):
                         tempCopy = deepcopy(schedule)
@@ -70,30 +116,30 @@ def generate_domain(course: Course, schedule: Schedule) -> List[Schedule]:
                     tempCopy = deepcopy(schedule)
                     tempCopy.schedule[row][column] = course.name
                     domain.append(tempCopy)
-    return domain
+    return domain """
 
-class TempConstraint(Constraint[Course, list[Schedule]]):
-    def __init__(self, variables: list[Schedule]):
-        self.variables: list[Schedule] = variables
+# class TempConstraint(Constraint[Course, list[Schedule]]):
+#     def __init__(self, variables: list[Schedule]):
+#         self.variables: list[Schedule] = variables
 
-    def satisfied(self, variableDict: Dict):
-        #print(variableDict)
-        stuff = [locs for values in variableDict.values() for locs in values]
-        schedule = Schedule([["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]])
-        for z in stuff:
-            for i in range(len(schedule)):
-                for y in range(len(schedule[0])):
-                    if z[i][y] != "-" and schedule[i][y] == "-":
-                        schedule[i][y] = z[i][y]
-                    else:
-                        if schedule[i][y] != "-":
-                            return False
-        return True
+#     def satisfied(self, variableDict: Dict):
+#         #print(variableDict)
+#         stuff = [locs for values in variableDict.values() for locs in values]
+#         schedule = Schedule([["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]])
+#         for z in stuff:
+#             for i in range(len(schedule)):
+#                 for y in range(len(schedule[0])):
+#                     if z[i][y] != "-" and schedule[i][y] == "-":
+#                         schedule[i][y] = z[i][y]
+#                     else:
+#                         if schedule[i][y] != "-":
+#                             return False
+#         return True
 
 
 class ScheduleConstraint(Constraint[Course, List[Schedule]]):
     # can't have same class twice on same day
-    # can't have same class twice on same time
+    # can't have same class twice on same time slot
     # can't have overlapping classes in the same room
     def __init__(self, variables: List[Course]) -> None:
         self.variables: List[Course] = variables
@@ -107,39 +153,14 @@ class ScheduleConstraint(Constraint[Course, List[Schedule]]):
                     for x in range(len(i)):
                         for y in range(len(i[0])):
                             if i[x][y] != "-" and j[x][y] != "-":
+                                if i[x][y].room == j[x][y].room:
+                                    return False
                                 return False
         return True
-        
-    # def satisfied(self, assignment: Dict[str, List[GridLocation]]) -> bool:
-    #     # constraint = [pos for values in assignment.values() for pos in values]
-    #     all_locations = [locs for values in assignment.values() for locs in values]
-    #     #FROM BOOK 
-    #     return len(set(all_locations)) == len(all_locations)
-
-# class RoomConstraint(Constraint[Chip, List[GridLocation]]):
-#     def __init__(self, chips: List[Chip]) -> None:
-#         super().__init__(chips)
-        
-#     def satisfied(self, assignment: Dict[str, List[GridLocation]]) -> bool:
-#         # constraint = [pos for values in assignment.values() for pos in values]
-#         all_locations = [locs for values in assignment.values() for locs in values]
-#         #FROM BOOK 
-#         return len(set(all_locations)) == len(all_locations)
-
-# class CourseConstraint(Constraint[Chip, List[GridLocation]]):
-#     def __init__(self, chips: List[Chip]) -> None:
-#         super().__init__(chips)
-        
-#     def satisfied(self, assignment: Dict[str, List[GridLocation]]) -> bool:
-#         # constraint = [pos for values in assignment.values() for pos in values]
-#         all_locations = [locs for values in assignment.values() for locs in values]
-#         #FROM BOOK 
-#         return len(set(all_locations)) == len(all_locations)
 
 def solution() -> None:
     # generate grid
     schedule = generate_grid(2, 5)
-    display_grid(schedule)
     # generate domain
     # generate constraints
     # generate CSP
@@ -149,7 +170,8 @@ def solution() -> None:
 
 if __name__ == "__main__":
     newSchedule = Schedule([["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]])
-    variables = [Course("300", Room("joyce110"), True , Professor("david")), Course("200", Room("joyce110"), True, Professor("david")), Course("400", Room("joyce110"), True, Professor("david"))]
+    # variables = [Course("300", Room("joyce110"), True , Professor("david")), Course("200", Room("joyce110"), True, Professor("david")), Course("400", Room("joyce110"), True, Professor("david"))]
+    variables = [Course("300", True), Course("200", False), Course("400", True)]
     variableDict = {}
     #print(generate_domain(variables[0], newSchedule))
     for x in variables:
@@ -168,3 +190,5 @@ if __name__ == "__main__":
     else:
         print("got something back")
         print(possibleOutcome)
+        #display_grid(dict_to_schedule(variables, possibleOutcome, newSchedule))
+        
